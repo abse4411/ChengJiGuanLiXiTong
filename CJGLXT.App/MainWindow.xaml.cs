6da1bf91eb24b99003b9b8387c211e4170cf070a
Shen Zhifeng <shen_zhifeng@outlook.com>
2019-10-02 15:23:58 +0800

    test

--- CJGLXT.App/MainWindow.xaml.cs
+++ CJGLXT.App/MainWindow.xaml.cs
@@ -53,6 +53,43 @@ namespace CJGLXT.App
                 Application.Current.Shutdown();
             }
         }
+
+        private void Exit(object sender, RoutedEventArgs e)
+        {
+            ServiceLocator.DisposeCurrent();
+            Application.Current.Shutdown();
+        }
+
+        private void About(object sender, RoutedEventArgs e)
+        {
+            var an = "该软件使用了以下开源的技术/软件,我们一直遵守各项使用协议。" + Environment.NewLine;
+            var mdt = "Name: MaterialDesignThemes" + Environment.NewLine
+                + "SourceCode: https://github.com/MaterialDesignInXAML/MaterialDesignInXamlToolkit" + Environment.NewLine
+                + "Version: 2.6.0" + Environment.NewLine
+                + "License: MIT" + Environment.NewLine
+                + "Copyright 2015 James Willock/Mulholland Software Ltd" + Environment.NewLine + Environment.NewLine;
+            var efc = "Name: Microsoft.EntityFrameworkCore" + Environment.NewLine
+                                                   + "SourceCode: https://github.com/aspnet/EntityFrameworkCore" + Environment.NewLine
+                                                   + "Version: 2.2.6" + Environment.NewLine
+                                                   + "License: Apache-2.0" + Environment.NewLine
+                                                   + "© Microsoft Corporation. All rights reserved." + Environment.NewLine + Environment.NewLine;
+            var efcss = "Name: Microsoft.EntityFrameworkCore.SqlServer" + Environment.NewLine
+                                                            + "SourceCode: https://github.com/aspnet/EntityFrameworkCore" + Environment.NewLine
+                                                            + "Version: 2.2.6" + Environment.NewLine
+                                                            + "License: Apache-2.0" + Environment.NewLine
+                                                            + "© Microsoft Corporation. All rights reserved." + Environment.NewLine + Environment.NewLine;
+            var dj = "Name: Microsoft.Extensions.DependencyInjection" + Environment.NewLine
+                                                                        + "SourceCode: https://github.com/aspnet/Extensions" + Environment.NewLine
+                                                                        + "Version: 2.2.0" + Environment.NewLine
+                                                                        + "License: Apache-2.0" + Environment.NewLine
+                                                                        + "© Microsoft Corporation. All rights reserved." + Environment.NewLine+ Environment.NewLine;
+            var va = "Name: XAML VanArsdel Inventory Sample" + Environment.NewLine
+                                                                      + "SourceCode: https://github.com/Microsoft/InventorySample" + Environment.NewLine
+                                                                      + "Version: 1.2.7.0" + Environment.NewLine
+                                                                      + "License: MIT" + Environment.NewLine
+                                                                      + "Copyright (c) Microsoft Corporation. All rights reserved." + Environment.NewLine + Environment.NewLine;
+            MessageBox.Show(an+mdt + efc + efcss + dj+va, "技术致谢");
+        }
     }
 
 }
